ae117be83bd82534205a4862ee8de2fdebdead56
SmashingBumpkin <66125276+SmashingBumpkin@users.noreply.github.com>
2023-07-04 16:13:14 +0200

    Connects to alphadex testnet + basic data
    Simple connection to the alphadex websocket.
    
    Fairly similar to what's being done in the tutorial, with a few revisions to work with nextjs, and a bit more data.

---
+++ src/app/PairInfo.tsx
@@ -0,0 +1,31 @@
+import { useContext } from "react"
+import { AdexStateContext } from "./page"
+import { OrderBook } from "./OrderBook"
+
+export function PairInfo() {
+    const adexState = useContext(AdexStateContext);
+    const { buys, sells } = adexState.currentPairOrderbook;
+
+    return (
+        <div>
+            <h4> Current pair {adexState.currentPairInfo.name}</h4>
+            <p>Address: {adexState.currentPairInfo.address}</p>
+            <p>Last Price: {adexState.currentPairInfo.lastPrice}</p>
+            <h4>Orderbook:</h4>
+
+            <table className="table">
+                <thead>
+                <tr>
+                    <th>Type</th>
+                    <th>Price</th>
+                    <th>Quantity Remaining</th>
+                    <th>Value Remaining</th>
+                </tr>
+                </thead>
+
+                <OrderBook orders={buys} type="Buy" />
+                <OrderBook orders={sells} type="Sell" />
+            </table>
+        </div>
+    )
+}
