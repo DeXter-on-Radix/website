81101e4258924d58a4e244fe468b9e998ff516ad
SmashingBumpkin <66125276+SmashingBumpkin@users.noreply.github.com>
2023-07-05 11:11:22 +0200

    Code formatting and comments

--- src/app/PairsList.tsx
+++ src/app/PairsList.tsx
@@ -3,23 +3,22 @@ import { AdexStateContext } from "./page";
 import * as adex from "alphadex-sdk-js";
 
 export function PairsList() {
-    const adexState = useContext(AdexStateContext);
-    const selectPair = (pairAddress) => {
-        adex.clientState.currentPairAddress = pairAddress;
-    }
-    return (
-        <div>
-            <h4>AlphaDEX Pairs</h4>
-            <ul>
-                {adexState.pairsList
-                .map((pair, index) => 
-                    <li key={index}>
-                    <button onClick={() => selectPair(pair.address)}>
-                        {pair.name}
-                    </button>
-                    </li>)
-                }
-            </ul>
-        </div>
-    )
+  const adexState = useContext(AdexStateContext);
+  const selectPair = (pairAddress) => {
+    adex.clientState.currentPairAddress = pairAddress;
+  };
+  return (
+    <div>
+      <h4>AlphaDEX Pairs</h4>
+      <ul>
+        {adexState.pairsList.map((pair, index) => (
+          <li key={index}>
+            <button onClick={() => selectPair(pair.address)}>
+              {pair.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
