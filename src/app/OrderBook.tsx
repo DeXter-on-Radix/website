70153fb35cfa3d8a508fe677c817baee48d1616a
SmashingBumpkin <66125276+SmashingBumpkin@users.noreply.github.com>
2023-07-06 15:22:44 +0200

    orders given explicit type
    Also pretty ^_^

--- src/app/OrderBook.tsx
+++ src/app/OrderBook.tsx
@@ -1,20 +1,30 @@
 import { useContext } from "react";
 import { AdexStateContext } from "./page";
+import { OrderbookLine } from "alphadex-sdk-js";
 
-export function OrderBook({ orders }) {
+export function OrderBook({ orders }: { orders: OrderbookLine[] }) {
   //returns simple orderbook of buys/sells
   const adexState = useContext(AdexStateContext);
   const { buys, sells } = adexState.currentPairOrderbook;
   return (
     <>
-      {orders.map((item: { price: number ; quantityRemaining: number ; valueRemaining: number ; }, index: number ) => (
-        <tr key={index}>
-          <th>{orders === buys ? "Buy" : "Sell"}</th>
-          <td>{item.price}</td>
-          <td>{item.quantityRemaining}</td>
-          <td>{item.valueRemaining}</td>
-        </tr>
-      ))}
+      {orders.map(
+        (
+          item: {
+            price: number;
+            quantityRemaining: number;
+            valueRemaining: number;
+          },
+          index: number
+        ) => (
+          <tr key={index}>
+            <th>{orders === buys ? "Buy" : "Sell"}</th>
+            <td>{item.price}</td>
+            <td>{item.quantityRemaining}</td>
+            <td>{item.valueRemaining}</td>
+          </tr>
+        )
+      )}
     </>
   );
 }
