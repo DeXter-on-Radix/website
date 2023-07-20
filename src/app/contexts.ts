cc95fc737248c4c8d2946646d4619d80835f0bb3
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2023-07-20 18:43:37 +0400

    fixes document not defined error

--- src/app/contexts.ts
+++ src/app/contexts.ts
@@ -1,42 +1,9 @@
-import { RadixDappToolkit } from "@radixdlt/radix-dapp-toolkit";
+import { State } from "@radixdlt/radix-dapp-toolkit";
 import { createContext } from "react";
 import { StaticState, clientState } from "alphadex-sdk-js";
 
-// Radix Dapp Toolkit init and field contexts
-
-export const rdt = RadixDappToolkit(
-  {
-    dAppDefinitionAddress:
-      "account_tdx_c_1pyc6tpqu2uy7tzy82cgm5c289x7qy6xehtkqe0j2yycsr9ukkl",
-    dAppName: "DeXter",
-  },
-  (requestData) => {
-    requestData({
-      accounts: { quantifier: "atLeast", quantity: 1 },
-    });
-  },
-  {
-    networkId: 12,
-    onDisconnect: () => {
-      // clear your application state
-    },
-    onInit: ({ accounts }) => {
-      // set your initial application state
-    },
-  }
-);
-
-export const RdtAccountsContext = createContext<
-  | {
-      address: string;
-      label: string;
-      appearanceId: number;
-    }[]
-  | undefined
->(undefined);
-
-// AlphaDEX State context
+export const RadixContext = createContext<State | undefined>(undefined);
 
 export const initialStaticState = new StaticState(clientState.internalState);
 
-export const AdexStateContext = createContext(initialStaticState);
+export const AdexStateContext = createContext<StaticState>(initialStaticState);
