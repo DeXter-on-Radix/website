35804507055802dca776f50dc2eafdb206e50c90
SmashingBumpkin <66125276+SmashingBumpkin@users.noreply.github.com>
2023-07-24 16:27:30 +0200

    Some simple formatting changes + swapQuote

--- src/app/NewOrder.tsx
+++ src/app/NewOrder.tsx
@@ -11,6 +11,17 @@ import { useAccounts } from "./hooks/useAccounts";
 import { useConnected } from "./hooks/useConnected";
 import { GatewayApiClient } from "@radixdlt/babylon-gateway-api-sdk";
 import { useRdt } from "./hooks/useRdt";
+import { SdkResult } from "alphadex-sdk-js/lib/models/sdk-result";
+
+const debounce = (callback: { (): void; (arg0: any): void; }, delay: number) => {
+  let timerId;
+  return (...args: any) => {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+};
 
 export function NewOrder() {
   const [gatewayApi, setGatewayApi] = useState<GatewayApiClient | null>(null);
@@ -39,27 +50,19 @@ export function NewOrder() {
   const [orderToken, setOrderToken] = useState<adex.TokenInfo>(
     adexState.currentPairInfo.token1
   );
-  const [positionSize, setPositionSize] = useState<number | undefined>(
-    undefined
-  );
-  const [price, setPrice] = useState<number | undefined>(undefined);
-  const [slippage, setSlippage] = useState<number | undefined>(undefined);
+  const [positionSize, setPositionSize] = useState<number>(-1);
+  const [price, setPrice] = useState<number>(-1);
+  const [slippage, setSlippage] = useState<number>(-1);
+  const [swapQuote, setSwapQuote] = useState<adex.SwapQuote | null>(null);
 
-  const createTx = ({
-    orderType,
-    side,
-    tokenAddress,
-    amount,
-    price,
-    slippage,
-  }: {
-    orderType: string;
-    side: string;
-    tokenAddress: string;
-    amount: number;
-    price: number;
-    slippage: number;
-  }) => {
+  const createTx = (
+    orderType: string,
+    side: string,
+    tokenAddress: string,
+    amount: number,
+    price: number,
+    slippage: number
+  ) => {
     price = orderType === "MARKET" ? -1 : price;
     console.log(
       "ORDER INPUT DETAILS:\n Pair %s\nType %s \nSide %s \n token address %s\namount %f\nprice %f\nslip %f\nfrontend%f\naccount %s\naccount %s",
@@ -114,20 +117,19 @@ export function NewOrder() {
     ) {
       // TODO: check fo null or replace with another API
       let response =
-        await gatewayApi.state.innerClient.entityFungibleResourceVaultPage({
+        await gatewayApi?.state.innerClient.entityFungibleResourceVaultPage({
           stateEntityFungibleResourceVaultsPageRequest: {
             address: account,
             /* eslint-disable */ resource_address /* eslint-enable*/:
               resourceAddress,
           },
         });
-      return response;
+      return response ? response : null;
     }
     return new Promise((resolve, reject) => {
       getResourceDetails(resourceAddress, account)
         .then((response) => {
-          const output = parseFloat(response.items[0].amount);
-          console.log("BALANCE %f", output);
+          const output = parseFloat(response ? response.items[0].amount : "0");
           resolve(output);
         })
         .catch((reason: TypeError) => {
@@ -153,15 +155,17 @@ export function NewOrder() {
       }
     },
     [
-      adexState.currentPairInfo.token1.address,
-      adexState.currentPairInfo.token2.address,
+      adexState.currentPairAddress,
       connected,
       accounts,
+      getAccountResourceBalance,
     ]
   );
 
+  //Updates token balances
   useEffect(() => {
     const account = accounts.length > 0 ? accounts[0].address : "";
+    console.log("Updating token pair/connecting");
     if (
       adexState.currentPairInfo.token1.address &&
       adexState.currentPairInfo.token2.address &&
@@ -173,21 +177,17 @@ export function NewOrder() {
         account
       );
     }
-    return;
   }, [
     adexState.currentPairInfo.token1.address,
     adexState.currentPairInfo.token2.address,
     connected,
     accounts,
-    fetchBalances,
   ]);
 
+  //Updates selected side (token1/token2)
   useEffect(() => {
     setOrderToken(adexState.currentPairInfo.token1);
-  }, [
-    adexState.currentPairInfo.token1,
-    adexState.currentPairInfo.token2,
-  ]);
+  }, [adexState.currentPairInfo.token1, adexState.currentPairInfo.token2]);
 
   function activeTypeTabClass(tabsOrderType: adex.OrderType) {
     let className = "tab tab-bordered";
@@ -203,9 +203,45 @@ export function NewOrder() {
     if (orderToken === tabsToken) {
       className += " tab-active";
     }
-
     return className;
   }
+
+  const getSwapQuote = () => {
+    const platformFee = 0.001; //TODO: Get this data from the platform badge and set it as a global variable
+    const adexPairInfo = adexState.currentPairInfo;
+    console.log("getswapquote posn size",positionSize);
+    console.log(
+      orderToken.address,
+      positionSize,
+      orderToken === adexPairInfo.token1
+        ? adexPairInfo.token2.address
+        : adexPairInfo.token1.address,
+      1,
+      platformFee
+    );
+    const quote = adex.getSwapQuote(
+      orderToken.address,
+      positionSize,
+      orderToken === adexPairInfo.token1
+        ? adexPairInfo.token2.address
+        : adexPairInfo.token1.address,
+      1,
+      platformFee
+    );
+    quote
+      .then((response) => {
+        setSwapQuote(response.data[0]);
+        console.log(swapQuote);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    const debouncedGetSwapQuote = debounce(getSwapQuote, 2000);
+    debouncedGetSwapQuote();
+  }, [positionSize]);
 
   return (
     <div>
@@ -259,87 +295,107 @@ export function NewOrder() {
             Position size
           </label>
           <input
-            type="decimal"
+            type="number"
             id="amount"
             name="amount"
             required
             className="w-full m-2 p-2 rounded-none"
-            onInput={(event) =>
-              setPositionSize(parseFloat(event.currentTarget.value))
-            }
+            onInput={(event) => {
+              setPositionSize(parseFloat(event.currentTarget.value));
+            }}
           />
         </div>
-
-        <div className="flex justify-between">
-          <label htmlFor="price" className="join-item my-auto">
-            Price
-          </label>
-          <input
-            type="decimal"
-            id="price"
-            name="price"
-            className="w-full m-2 p-2 rounded-none"
-            onInput={(event) => setPrice(parseFloat(event.currentTarget.value))}
-          />
-        </div>
-
-        <div className="flex">
-          <label htmlFor="slippage" className="my-auto">
-            Slippage
-          </label>
-          <input
-            type="decimal"
-            id="slippage"
-            name="slippage"
-            className="w-full m-2 p-2 rounded-none"
-            onInput={(event) =>
-              setSlippage(parseFloat(event.currentTarget.value))
-            }
-          />
-        </div>
+        {orderType !== adex.OrderType.MARKET && (
+          <div>
+            <div className="flex justify-between">
+              <label htmlFor="price" className="join-item my-auto">
+                Price
+              </label>
+              <input
+                type="number"
+                id="price"
+                name="price"
+                className="w-full m-2 p-2 rounded-none"
+                onInput={(event) =>
+                  setPrice(parseFloat(event.currentTarget.value))
+                }
+              />
+            </div>
+            <div className="flex">
+              <label htmlFor="slippage" className="my-auto">
+                Slippage
+              </label>
+              <input
+                type="number"
+                id="slippage"
+                name="slippage"
+                className="w-full m-2 p-2 rounded-none"
+                onInput={(event) =>
+                  setSlippage(parseFloat(event.currentTarget.value))
+                }
+              />
+            </div>
+          </div>
+        )}
+        {swapQuote && (
+          <div>
+            <p>Display swap quote data here </p>
+          </div>
+        )}
       </div>
       <br />
       {connected && (
-        <button
-          className="btn m-2"
-          onClick={() => {
-            if (!price || !positionSize || !slippage) {
-              alert("Please fill out all fields");
-              return;
-            }
-            return createTx({
-              orderType,
-              side: adex.OrderSide.BUY,
-              tokenAddress: orderToken.address,
-              amount: positionSize,
-              price,
-              slippage,
-            });
-          }}
-        >
-          Buy {orderToken.name}
-        </button>
-      )}
-      {connected && (
-        <button
-          className="btn m-2 mb-8"
-          onClick={() => {
-            if (!price || !positionSize || !slippage) {
-              alert("Please fill out all fields");
-              return;
-            }
-            return createTx({
-              orderType,
-              side: adex.OrderSide.SELL,
-              tokenAddress: orderToken.address,
-              amount: positionSize,
-              price,
-              slippage,
-            });
-          }}
-        >
-          Sell {orderToken.name}
-        </button>
+        <div>
+          <button
+            className="btn m-2"
+            onClick={() => {
+              if (
+                !positionSize ||
+                (orderType !== adex.OrderType.MARKET &&
+                  (price < 0 || slippage < 0))
+              ) {
+                alert("Please fill out all fields");
+                return;
+              }
+              return createTx(
+                orderType,
+                adex.OrderSide.BUY,
+                orderToken.address,
+                positionSize,
+                price,
+                slippage
+              );
+            }}
+          >
+            Buy {orderToken.name}
+          </button>
+          <button
+            className="btn m-2 mb-8"
+            onClick={() => {
+              if (
+                !positionSize ||
+                (orderType !== adex.OrderType.MARKET &&
+                  (price < 0 || slippage < 0))
+              ) {
+                alert("Please fill out all fields");
+                return;
+              }
+              return createTx(
+                orderType,
+                adex.OrderSide.SELL,
+                orderToken.address,
+                positionSize,
+                price,
+                slippage
+              );
+            }}
+          >
+            Sell {orderToken.name}
+          </button>
+          <button className="btn m-2 mb-8" onClick={() => {}}>
+            quote
+          </button>
+        </div>
       )}
     </div>
   );
