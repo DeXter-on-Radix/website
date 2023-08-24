3f6b5694f4c2f4bff83bef9b3e0df8744948fd7b
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2023-08-24 11:55:28 +0400

    update adex get old api version in response

--- src/app/subscriptions.ts
+++ src/app/subscriptions.ts
@@ -50,6 +50,8 @@ export function initializeSubscriptions(store: AppStore) {
         JSON.stringify(newState)
       );
 
+      console.log("newState", serializedState);
+
       store.dispatch(pairSelectorSlice.actions.updateAdex(serializedState));
       store.dispatch(orderBookSlice.actions.updateAdex(serializedState));
       store.dispatch(updateCandles(serializedState.currentPairCandlesList));
