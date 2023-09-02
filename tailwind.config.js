5ad499ed0f2bdc04527f511cfda6f8ac964650ac
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2023-09-02 18:07:39 +0500

    config: daisyui dark theme colors palette setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,35 @@ module.exports = {
       },
     },
   },
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          primary: "#292C35",
+          "primary-content": "#F6F6F7",
+          secondary: "#5C5E66",
+          "secondary-content": "#F6F6F7",
+          accent: "#BFED3F",
+          "accent-content": "#12141C",
+          neutral: "#292C34",
+          error: "#FF7A75",
+          success: "#73D2BD",
+          "base-100": "#292C35",
+          "base-300": "#12141C",
+          "base-content": "#F6F6F7",
+          "--rounded-btn": "0.2rem",
+          "--btn-text-case": "none",
+        },
+      },
+    ], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+  },
   plugins: [require("daisyui"), require("@tailwindcss/typography")],
 };
