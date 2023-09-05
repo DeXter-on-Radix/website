6a3faa5401f3cbfb6578695d2b8c1c4e26e39ae4
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2023-09-05 16:58:31 +0500

    config: adding xs (extra -small) screen breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        xs: "320px",
+      },
     },
   },
   daisyui: {
