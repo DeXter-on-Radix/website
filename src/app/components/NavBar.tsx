a7308fcb6342f74581517a6c3b8bf39fb70a08b2
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2023-09-06 16:11:57 +0400

    logo icon in the navbar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Image from "next/image";
 
 export function Navbar() {
   return (
     <div className="col-span-12 navbar">
       <div className="navbar-start">
-        <a className="btn btn-ghost normal-case text-xl">DeXter</a>
+        <a
+          href="/"
+          className="btn btn-ghost hover:bg-transparent no-underline text-xl p-0"
+        >
+          <Image
+            src="/logo_icon.svg"
+            alt="Dexter Logo"
+            width={40}
+            height={40}
+            className="!my-0"
+          />
+          <span className="xs:invisible sm:visible uppercase">DeXter</span>
+        </a>
       </div>
       <div className="navbar-end">
         <radix-connect-button className="btn"></radix-connect-button>
