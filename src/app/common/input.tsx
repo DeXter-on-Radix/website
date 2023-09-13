85840eb05593ecfe32feaecdfec00ff301830363
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2023-09-14 01:02:00 +0500

    feat: reusable input component

---
+++ src/app/common/input.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from "react";
+
+type Props = Omit<JSX.IntrinsicElements["input"], "className"> & {
+  parentClasses?: string;
+  inputClasses?: string;
+  endAdornmentClasses?: string;
+  endAdornment?: ReactNode;
+  isError?: boolean;
+};
+
+export const Input = ({
+  parentClasses = "",
+  inputClasses = "",
+  endAdornment,
+  isError = false,
+  endAdornmentClasses = "",
+  ...inputProps
+}: Props) => {
+  return (
+    <div
+      className={
+        "flex flex-row items-center bg-base-300 " +
+        (isError ? "border border-error " : "") +
+        parentClasses
+      }
+    >
+      <input className={"input w-full px-2 " + inputClasses} {...inputProps} />
+      {endAdornment && (
+        <div className={"px-3 " + endAdornmentClasses}>{endAdornment}</div>
+      )}
+    </div>
+  );
+};
