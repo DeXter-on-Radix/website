4ab7ae84fed49c67dbebdeca722319ab592bbde3
mael <107124432+maelsar@users.noreply.github.com>
2023-09-13 19:16:36 +0800

    applied styling colors for values of price info

--- src/app/components/PriceInfo.tsx
+++ src/app/components/PriceInfo.tsx
@@ -8,32 +8,60 @@ export function PriceInfo() {
   const high = priceInfo.high24h;
   const low = priceInfo.low24h;
   const volume = priceInfo.value24h;
+  const isNegativeOrZero = priceInfo.isNegativeOrZero;
   //   const open = priceInfo.open24h;
 
   return (
     <div className="flex justify-between py-2">
-      {" "}
       <div className="flex flex-col items-start pl-8">
-        {" "}
         <span className="text-sm">Price</span>
-        <span className="text-xs">{lastPrice}</span>{" "}
+        <span
+          className={
+            isNegativeOrZero ? "text-xs text-red-500" : "text-xs text-green-500"
+          }
+        >
+          {lastPrice}
+        </span>
       </div>
       <div className="flex flex-col items-start">
         <span className="text-sm">24h Change</span>
-        <span className="text-xs">{change}</span>
+        <span
+          className={
+            isNegativeOrZero ? "text-xs text-red-500" : "text-xs text-green-500"
+          }
+        >
+          {change}
+        </span>
       </div>
       <div className="flex flex-col items-start">
         <span className="text-sm">24h High</span>
-        <span className="text-xs">{high}</span>
+        <span
+          className={
+            isNegativeOrZero ? "text-xs text-red-500" : "text-xs text-green-500"
+          }
+        >
+          {high}
+        </span>
       </div>
       <div className="flex flex-col items-start">
         <span className="text-sm">24h Low</span>
-        <span className="text-xs">{low}</span>
+        <span
+          className={
+            isNegativeOrZero ? "text-xs text-red-500" : "text-xs text-green-500"
+          }
+        >
+          {low}
+        </span>
       </div>
       <div className="flex flex-col items-start pr-8">
-        {" "}
         <span className="text-sm">24h Volume</span>
-        <span className="text-xs">{volume}</span>
+        <span
+          className={
+            isNegativeOrZero ? "text-xs text-red-500" : "text-xs text-green-500"
+          }
+        >
+          {volume}
+        </span>
       </div>
     </div>
   );
