9bbbdb14808a380a309149ed53e7a058062d77bf
Fred Liebenberg <fliebenberg@gmail.com>
2023-10-23 10:30:47 +0200

    removed uislice from store

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -6,11 +6,9 @@ import { priceChartSlice } from "./priceChartSlice";
 import { accountHistorySlice } from "./accountHistorySlice";
 import { radixSlice } from "./radixSlice";
 import { priceInfoSlice } from "./priceInfoSlice";
-import { uiSlice } from "./uiSlice";
 
 export const store = configureStore({
   reducer: {
-    ui: uiSlice.reducer,
     radix: radixSlice.reducer,
     pairSelector: pairSelectorSlice.reducer,
     orderInput: orderInputSlice.reducer,
