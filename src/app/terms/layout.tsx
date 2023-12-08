7a9b762c1c1107df541b51e03496465ec0d2fed9
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2023-12-08 20:29:46 +0400

    more information on fees
    remove prohibitions of reusing an modifying the contents of the site
    add link to Radix privacy policy

--- src/app/terms/layout.tsx
+++ src/app/terms/layout.tsx
@@ -1,4 +1,4 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   // Create any shared layout or styles here
-  return <div className="max-w-screen-lg mx-auto px-4">{children}</div>;
+  return <div className="max-w-screen-lg mx-auto p-4">{children}</div>;
 }
