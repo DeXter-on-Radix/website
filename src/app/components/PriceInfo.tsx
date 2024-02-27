b119ce536c06466d76d02d33cc6346a5791c5df1
dcts <thomas@dcts.dev>
2024-02-27 01:21:51 +0100

    remove unneeded improvement

--- src/app/components/PriceInfo.tsx
+++ src/app/components/PriceInfo.tsx
@@ -15,7 +15,7 @@ export function PriceInfo() {
   const basePair = "XRD";
 
   return (
-    <div className="flex justify-between py-2 h-full max-w-[650px] lg:max-w-none ">
+    <div className="flex justify-between py-2 h-full ">
       <div className="flex flex-col items-start justify-center pl-5 lg:pl-8">
         <span className="text-sm font-bold text-secondary-content uppercase">
           Price
