5b69e4c8e9b67751c9dd660ef10d6a51fabbfba8
dcts <thomas@dcts.dev>
2024-03-28 09:36:41 +0100

    reverse price chart size

--- src/app/components/PriceChart.tsx
+++ src/app/components/PriceChart.tsx
@@ -80,7 +80,7 @@ function PriceChartCanvas(props: PriceChartProps) {
 
       const chart = createChart(chartContainer, {
         width: chartContainer.clientWidth,
-        height: 540,
+        height: 500,
         layout: {
           background: {
             color: theme["base-200"],
