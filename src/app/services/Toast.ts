fdffdb61e1c2d327729157994a02927aebd248ad
dcts <thomas@dcts.dev>
2024-04-01 04:05:20 +0200

    add comment/documentation

--- src/app/services/Toast.ts
+++ src/app/services/Toast.ts
@@ -1,7 +1,15 @@
 import { toast } from "react-hot-toast";
 
+/**
+ * Wrapper service for 'react-hot-toast' notifications to generate
+ * dexter branded toast notifications.
+ * https://react-hot-toast.com/
+ */
 export class Toast {
   static helloWorld() {
     toast.success("hello world");
+  }
+  static success(message: string) {
+    toast.success(message);
   }
 }
