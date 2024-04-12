cb281c7c993e0ee3b824ee7a63c2eb9e821babf4
dcts <thomas@dcts.dev>
2024-04-12 15:51:58 +0200

    content alignment for desktop mostly done

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -57,6 +57,26 @@ export function Footer() {
       },
     ],
   };
+
+  return (
+    <footer className="p-4 bg-base-300 text-xs text-secondary-content px-12 pt-8 pb-16">
+      <div className="flex justify-between">
+        <div className="flex">
+          <FirstFooterElement />
+          <FooterContentColumn {...contentColumn1} />
+          <FooterContentColumn {...contentColumn2} />
+          <FooterContentColumn {...contentColumn3} />
+        </div>
+        <div className="">
+          <h3>Our Community</h3>
+          <SocialIcons />
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+function SocialIcons() {
   const socials = [
     {
       id: "telegram",
@@ -88,82 +108,14 @@ export function Footer() {
     //   url: "",
     // },
   ];
-
   return (
-    <footer className="p-4 bg-base-300 text-xs text-secondary-content px-12 pt-8 pb-16">
-      <div className="flex">
-        <div>
-          <FirstFooterElement />
-          <FooterContentColumn {...contentColumn1} />
-          <FooterContentColumn {...contentColumn2} />
-          <FooterContentColumn {...contentColumn3} />
-        </div>
-        <div className="">
-          <h3>Our Community</h3>
-          {socials.map(({ id, url }, indx) => (
-            <a href={url} key={indx} target="_blank">
-              <img width="28" src={`/socials/${id}.svg`} alt={id}></img>
-            </a>
-          ))}
-        </div>
-      </div>
-    </footer>
-    /* <footer className="col-span-12 p-4 grid grid-cols-1 grid-rows-5 gap-4 bg-base-300 text-xs text-secondary-content lg:grid-cols-3 lg:grid-rows-3 lg:p-12 lg:gap-8">
-      <div className="flex flex-col space-y-4 px-16 lg:row-span-2">
-        <div className="uppercase">{t("social_media")}</div>
-
-        <div className="flex justify-between">
-          <a
-            href="https://twitter.com/DexterOnRadix"
-            target="_blank"
-            className="text-secondary-content hover:text-primary-content"
-          >
-            <FaXTwitter size="1rem" />
-          </a>
-
-          <a
-            href="https://t.me/dexter_discussion"
-            target="_blank"
-            className="text-secondary-content hover:text-primary-content"
-          >
-            <FaTelegram size="1rem" />
-          </a>
-
-          <a
-            href="https://discord.gg/Y44jqe2q2W"
-            target="_blank"
-            className="text-secondary-content hover:text-primary-content"
-          >
-            <FaDiscord size="1rem" />
-          </a>
-
-          <a
-            href="https://github.com/DeXter-on-Radix"
-            target="_blank"
-            className="text-secondary-content hover:text-primary-content"
-          >
-            <FaGithub size="1rem" />
-          </a>
-
-          <a
-            href="https://dexter-on-radix.gitbook.io"
-            target="_blank"
-            className="text-secondary-content hover:text-primary-content"
-          >
-            <SiGitbook size="1rem" />
-          </a>
-        </div>
-      </div>
-
-      <div className="flex justify-end items-center space-x-4 lg:row-start-3 lg:col-start-3 lg:pr-8 uppercase">
-        <AccentLink
-          href="/terms"
-          originalClassName="whitespace-nowrap text-secondary-content"
-        >
-          {t("terms_and_conditions")}
-        </AccentLink>
-      </div>
-    </footer> */
+    <div className="flex">
+      {socials.map(({ id, url }, indx) => (
+        <a href={url} key={indx} target="_blank" className="mr-2">
+          <img width="34" src={`/socials/${id}.svg`} alt={id}></img>
+        </a>
+      ))}
+    </div>
   );
 }
 
@@ -180,16 +132,19 @@ function FirstFooterElement() {
       />
 
       <div className="">
-        {t("footer_3")} <br /> {t("footer_2")} <br />
-        {t("runs_on")}{" "}
-        <a href="https://www.radixdlt.com/" target="_blank">
-          RADIX
-        </a>{" "}
-        &{" "}
-        <a href="https://alphadex.net/" target="_blank">
-          ALPHA DEX
-        </a>
-        .
+        <p className="truncate">{t("footer_3")}</p>
+        <p className="truncate">{t("footer_2")}</p>
+        <p className="truncate">
+          {t("runs_on")}{" "}
+          <a href="https://www.radixdlt.com/" target="_blank">
+            RADIX
+          </a>{" "}
+          &{" "}
+          <a href="https://alphadex.net/" target="_blank">
+            ALPHA DEX
+          </a>
+          .
+        </p>
       </div>
 
       <div className="">
@@ -214,15 +169,14 @@ function FooterContentColumn({
   title = "",
   links = [],
 }: FooterContentColumnProps) {
-  console.log({ title, links });
   return (
-    <>
-      <h3>{title}</h3>
+    <div className="px-3">
+      <h3 style={{ margin: 0 }}>{title}</h3>
       {links.map((link, i) => (
-        <p className="text-secondary-content" key={i}>
+        <p className="text-secondary-content truncate" key={i}>
           {link.text}
         </p>
       ))}
-    </>
+    </div>
   );
 }
