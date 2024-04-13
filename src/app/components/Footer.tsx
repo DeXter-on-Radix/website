fc76ef65c9a5545fee78a907e529dafb232bfb72
dcts <thomas@dcts.dev>
2024-04-13 23:27:09 +0200

    responsiveness done

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,6 +3,8 @@ import { useTranslations } from "hooks";
 import "../styles/footer.css";
 import Image from "next/image";
 
+const marginBottom = "mb-8";
+
 export function Footer() {
   // TODO(dcts): add translations
   // const t = useTranslations();
@@ -59,9 +61,9 @@ export function Footer() {
   };
 
   return (
-    <footer className="p-4 bg-base-300 text-xs text-secondary-content px-12 pt-8 pb-16">
-      <div className="flex justify-between">
-        <div className="flex">
+    <footer className="bg-base-300 text-xs text-secondary-content px-12 pt-8 pb-16">
+      <div className="flex flex-wrap justify-between">
+        <div className="flex flex-wrap">
           <FirstFooterElement />
           <FooterContentColumn {...contentColumn1} />
           <FooterContentColumn {...contentColumn2} />
@@ -73,6 +75,66 @@ export function Footer() {
         </div>
       </div>
     </footer>
+  );
+}
+
+function FirstFooterElement() {
+  const t = useTranslations();
+  return (
+    <div className={`text-xs flex flex-col pr-10 ${marginBottom}`}>
+      <Image
+        src="/dexter-logo-and-lettering.svg"
+        alt="DeXter icon"
+        width="113"
+        height="22"
+        className="h-auto"
+      />
+
+      <div className="">
+        <p className="truncate pt-4">{t("footer_3")}</p>
+        <p className="truncate pt-2">{t("footer_2")}</p>
+        <p className="truncate">
+          {t("runs_on")}{" "}
+          <a href="https://www.radixdlt.com/" target="_blank">
+            RADIX
+          </a>{" "}
+          &{" "}
+          <a href="https://alphadex.net/" target="_blank">
+            ALPHA DEX
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  );
+}
+
+interface FooterContentColumnProps {
+  title: string;
+  links: { text: string; url: string }[];
+}
+
+function FooterContentColumn({
+  title = "",
+  links = [],
+}: FooterContentColumnProps) {
+  return (
+    <div className={`pr-8 ${marginBottom}`}>
+      <h3
+        style={{ margin: 0, fontSize: "14px" }}
+        className="py-2 text-secondary-content font-medium"
+      >
+        {title}
+      </h3>
+      {links.map((link, i) => (
+        <p
+          className="truncate  text-xs py-[2px] cursor-pointer text-white"
+          key={i}
+        >
+          {link.text}
+        </p>
+      ))}
+    </div>
   );
 }
 
@@ -102,80 +164,17 @@ function SocialIcons() {
       id: "youtube",
       url: "",
     },
-    // TODO(dcts): add gitbook
-    // {
-    //   id: "gitbook",
-    //   url: "",
-    // },
+    {
+      id: "gitbook",
+      url: "",
+    },
   ];
   return (
-    <div className="flex">
+    <div className={`flex flex-wrap ${marginBottom}`}>
       {socials.map(({ id, url }, indx) => (
-        <a href={url} key={indx} target="_blank" className="mr-2">
-          <img width="34" src={`/socials/${id}.svg`} alt={id}></img>
+        <a href={url} key={indx} target="_blank" className="mr-2 mt-2">
+          <img className="w-6 sm:w-7" src={`/socials/${id}.svg`} alt={id}></img>
         </a>
-      ))}
-    </div>
-  );
-}
-
-function FirstFooterElement() {
-  const t = useTranslations();
-  return (
-    <div className="text-xs flex flex-col ">
-      <Image
-        src="/dexter-logo-and-lettering.svg"
-        alt="DeXter icon"
-        width="113"
-        height="22"
-        className="h-auto"
-      />
-
-      <div className="">
-        <p className="truncate">{t("footer_3")}</p>
-        <p className="truncate">{t("footer_2")}</p>
-        <p className="truncate">
-          {t("runs_on")}{" "}
-          <a href="https://www.radixdlt.com/" target="_blank">
-            RADIX
-          </a>{" "}
-          &{" "}
-          <a href="https://alphadex.net/" target="_blank">
-            ALPHA DEX
-          </a>
-          .
-        </p>
-      </div>
-
-      <div className="">
-        <a href="https://www.radixdlt.com/" target="_blank">
-          <img
-            width="115"
-            src="runs-on-radix.png"
-            alt="Runs on Radix Button"
-          ></img>
-        </a>
-      </div>
-    </div>
-  );
-}
-
-interface FooterContentColumnProps {
-  title: string;
-  links: { text: string; url: string }[];
-}
-
-function FooterContentColumn({
-  title = "",
-  links = [],
-}: FooterContentColumnProps) {
-  return (
-    <div className="px-3">
-      <h3 style={{ margin: 0 }}>{title}</h3>
-      {links.map((link, i) => (
-        <p className="text-secondary-content truncate" key={i}>
-          {link.text}
-        </p>
       ))}
     </div>
   );
