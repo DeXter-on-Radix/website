1b4fa3c14644463ad072429c9a6ab6cf622104d3
dcts <thomas@dcts.dev>
2024-04-15 00:53:47 +0200

    navbar mobile MVP done

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
+import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
 import { useAppDispatch, useAppSelector } from "hooks";
 import { getSupportedLanguagesAsString } from "../state/i18nSlice";
@@ -10,6 +11,18 @@ import { i18nSlice } from "../state/i18nSlice";
 
 import Cookies from "js-cookie";
 import { usePathname } from "next/navigation";
+import { isMobile } from "../utils";
+
+const NavItems: { path: string; title: string }[] = [
+  {
+    path: "/",
+    title: "Trade",
+  },
+  {
+    path: "/rewards",
+    title: "Rewards",
+  },
+];
 
 export function Navbar() {
   return (
@@ -34,8 +47,15 @@ function NavbarContentDesktop() {
   return (
     <>
       <div className="hidden sm:flex h-full items-center flex-1 px-2 mx-2 mt-1 z-10">
-        <NavbarItem title="Trade" target="/" />
-        {/* <NavbarItem title="Rewards" target="/rewards" /> */}
+        {NavItems.map((navItem, indx) => {
+          return (
+            <NavbarItemDesktop
+              title={navItem.title}
+              target={navItem.path}
+              key={indx}
+            />
+          );
+        })}
       </div>
       <div className="hidden sm:flex p-2">
         <LanguageSelection />
@@ -48,8 +68,8 @@ function NavbarContentDesktop() {
 function NavbarContentMobile() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div className="sm:hidden flex justify-center items-center mr-4">
-      <button onClick={() => setMenuOpen(!menuOpen)}>
+    <div className="sm:hidden flex justify-center items-center mr-6">
+      <button onClick={() => setMenuOpen(true)}>
         <Image
           src="/hamburger-icon.svg"
           alt="menu"
@@ -59,12 +79,38 @@ function NavbarContentMobile() {
         />
       </button>
       {menuOpen && (
-        <div className="flex flex-col items-end pt-10 pr-12 w-[100vw] h-[100vh] bg-transparent overflow-hidden z-30 fixed top-0 left-0 backdrop-blur-xl">
-          <button onClick={() => setMenuOpen(false)}>CLOSE</button>
-          <div>Trade</div>
-          <div>Trade</div>
-          <radix-connect-button></radix-connect-button>
-          <LanguageSelection />
+        <div
+          className={`flex flex-col items-end w-[100vw] h-[100vh] bg-transparent overflow-hidden z-30 fixed top-0 left-0 backdrop-blur-lg py-4 ${
+            isMobile() ? "px-6" : "px-10"
+          }`}
+        >
+          <button onClick={() => setMenuOpen(false)}>
+            <Image
+              src="/close-x.svg"
+              alt="menu"
+              width="32"
+              height="32"
+              className="h-auto color-white opacity-70"
+            />
+          </button>
+          <div className="mt-10 w-full">
+            {NavItems.map((navItem, indx) => {
+              return (
+                <NavbarItemMobile
+                  title={navItem.title}
+                  target={navItem.path}
+                  setMenuOpen={setMenuOpen}
+                  key={indx}
+                />
+              );
+            })}
+          </div>
+          <div className="w-full flex flex-col items-center justify-center">
+            <h3 className="w-full text-center text-secondary-content font-light text-base">
+              Language Selection
+            </h3>
+            <LanguageSelection />
+          </div>
         </div>
       )}
     </div>
@@ -75,10 +121,12 @@ interface NavbarItemProps {
   title: string;
   target: string;
 }
+interface NavbarItemMobileProps extends NavbarItemProps {
+  setMenuOpen: (newMenuOpenState: boolean) => void;
+}
 
-function NavbarItem({ title, target }: NavbarItemProps) {
+function NavbarItemDesktop({ title, target }: NavbarItemProps) {
   const active = target === usePathname();
-
   return (
     <Link
       className={`h-full flex items-center px-5 hover:!no-underline hover:text-accent mb-0 ${
@@ -87,6 +135,29 @@ function NavbarItem({ title, target }: NavbarItemProps) {
       href={target}
     >
       <p className={`text-sm ${active ? "text-[#cafc40]" : "text-white"}`}>
+        {title}
+      </p>
+    </Link>
+  );
+}
+
+function NavbarItemMobile({
+  title,
+  target,
+  setMenuOpen,
+}: NavbarItemMobileProps) {
+  const active = target === usePathname();
+  return (
+    <Link
+      className={`my-2 hover:!no-underline`}
+      href={target}
+      onClick={() => setMenuOpen(false)}
+    >
+      <p
+        className={`text-2xl text-center py-4 ${
+          active ? "text-[#cafc40]" : "text-white"
+        }`}
+      >
         {title}
       </p>
     </Link>
@@ -108,7 +179,7 @@ function LanguageSelection() {
     <div className="mr-4 flex">
       {supportedLanguages.map((lang) => (
         <button
-          className={`uppercase text-sm px-1 ${
+          className={`uppercase py-2 px-2 sm:px-1 text-xl sm:text-sm ${
             language === lang ? "font-extrabold" : "font-extralight"
           }`}
           key={lang}
