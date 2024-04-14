ebb498e4b4f7169e6991a30510e3f2e9e2f91f0c
dcts <thomas@dcts.dev>
2024-04-14 17:07:08 +0200

    mobile hamburgermenu MVP working

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -13,7 +13,7 @@ import { usePathname } from "next/navigation";
 
 export function Navbar() {
   return (
-    <div className="flex items-center justify-between min-h-16 w-full h-20">
+    <nav className="flex items-center justify-between min-h-16 w-full h-20">
       <Image
         src="/dexter-logo-and-lettering.svg"
         alt="Dexter logo and lettering"
@@ -22,14 +22,51 @@ export function Navbar() {
         className="!my-0 pl-2 mx-3 p-2"
         priority={true}
       />
-      <div className="flex h-full items-center flex-1 px-2 mx-2 mt-1 z-10">
+      {/* DESKTOP VERSION */}
+      <NavbarContentDesktop />
+      {/* MOBILE VERSION (with hamburger menu) */}
+      <NavbarContentMobile />
+    </nav>
+  );
+}
+
+function NavbarContentDesktop() {
+  return (
+    <>
+      <div className="hidden sm:flex h-full items-center flex-1 px-2 mx-2 mt-1 z-10">
         <NavbarItem title="Trade" target="/" />
         {/* <NavbarItem title="Rewards" target="/rewards" /> */}
       </div>
-      <div className="flex p-2">
+      <div className="hidden sm:flex p-2">
         <LanguageSelection />
         <radix-connect-button></radix-connect-button>
       </div>
+    </>
+  );
+}
+
+function NavbarContentMobile() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <div className="sm:hidden flex justify-center items-center mr-4">
+      <button onClick={() => setMenuOpen(!menuOpen)}>
+        <Image
+          src="/hamburger-icon.svg"
+          alt="menu"
+          width="32"
+          height="32"
+          className="h-auto color-white"
+        />
+      </button>
+      {menuOpen && (
+        <div className="flex flex-col items-end pt-10 pr-12 w-[100vw] h-[100vh] bg-transparent overflow-hidden z-30 fixed top-0 left-0 backdrop-blur-xl">
+          <button onClick={() => setMenuOpen(false)}>CLOSE</button>
+          <div>Trade</div>
+          <div>Trade</div>
+          <radix-connect-button></radix-connect-button>
+          <LanguageSelection />
+        </div>
+      )}
     </div>
   );
 }
