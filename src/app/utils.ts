2b1cae72757e04d8719442769b58f968a200c13f
dcts <thomas@dcts.dev>
2024-04-15 01:31:57 +0200

    improve mobile detection helper

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -372,10 +372,22 @@ export function detectOperatingSystem(): OperatingSystem {
   }
 }
 
+// Detects mobile devices
 export function isMobile(): boolean {
   const userAgent = navigator.userAgent.toLowerCase();
-  // Check the user agent for mobile patterns
-  return /android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos/i.test(
-    userAgent
-  );
+  if (
+    userAgent.match(/Android/i) ||
+    userAgent.match(/webOS/i) ||
+    userAgent.match(/avantgo/i) ||
+    userAgent.match(/iPhone/i) ||
+    userAgent.match(/iPad/i) ||
+    userAgent.match(/iPod/i) ||
+    userAgent.match(/BlackBerry/i) ||
+    userAgent.match(/bolt/i) ||
+    userAgent.match(/Windows Phone/i) ||
+    userAgent.match(/Phone/i)
+  ) {
+    return true;
+  }
+  return false;
 }
