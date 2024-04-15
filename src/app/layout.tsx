281fd4641912c085e54653ce4b015c1172cff16a
dcts <thomas@dcts.dev>
2024-04-16 00:36:20 +0200

    show new assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ import { PromoBanner, PromoBannerProps } from "components/PromoBanner";
 // Configuration for promo banner
 // Once both images and a targetUrl are defined the banner will automatically show
 const promoBannerConfig: PromoBannerProps = {
-  imageUrl: "/promo-banners/validator-node-staking/desktop-1640x128.png",
-  imageUrlMobile: "/promo-banners/validator-node-staking/mobile-500x128.png",
+  imageUrl: "/promo-banners/validator-node-staking/desktop-600x80.svg",
+  imageUrlMobile: "/promo-banners/validator-node-staking/mobile-600x200.svg",
   redirectUrl:
     "https://dashboard.radixdlt.com/network-staking/validator_rdx1s0sr7xsr286jwffkkcwz8ffnkjlhc7h594xk5gvamtr8xqxr23a99a",
 };
