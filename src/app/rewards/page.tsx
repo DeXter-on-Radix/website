64ca3c728bbcb5c1e7f1128cd552fb3b348e4fb4
dcts <thomas@dcts.dev>
2024-04-17 02:23:49 +0200

    fix footer bug

--- src/app/rewards/page.tsx
+++ src/app/rewards/page.tsx
@@ -31,7 +31,7 @@ export default function Rewards() {
   }, []);
 
   return (
-    <div className="bg-[#141414] h-full">
+    <div className="bg-[#141414]">
       {/* <HeaderComponent /> */}
       <DebugStateLogger />
       <RewardsCard />
