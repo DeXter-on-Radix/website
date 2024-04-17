992e024a1cde507d36e813df2eb3d46ff446baf1
dcts <thomas@dcts.dev>
2024-04-17 08:59:18 +0200

    revert link to radix dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,8 @@ import { PromoBanner, PromoBannerProps } from "components/PromoBanner";
 const promoBannerConfig: PromoBannerProps = {
   imageUrl: "/promo-banners/validator-node-staking/desktop-600x80.svg",
   imageUrlMobile: "/promo-banners/validator-node-staking/mobile-600x200.svg",
-  redirectUrl: "https://twitter.com/DexterOnRadix/status/1780300821772607574",
+  redirectUrl:
+    "https://dashboard.radixdlt.com/network-staking/validator_rdx1s0sr7xsr286jwffkkcwz8ffnkjlhc7h594xk5gvamtr8xqxr23a99a",
 };
 
 export default function Home() {
