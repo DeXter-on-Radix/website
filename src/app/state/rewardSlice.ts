1a445435dda07bdae430aa2d051fac6ed9e116fd
dcts <thomas@dcts.dev>
2024-04-19 02:38:37 +0200

    fix successUi shown on load

--- src/app/state/rewardSlice.ts
+++ src/app/state/rewardSlice.ts
@@ -73,7 +73,7 @@ const initialState: RewardState = {
       },
     },
   },
-  showSuccessUi: true,
+  showSuccessUi: false,
 };
 
 type NonFungibleResource = NonFungibleResourcesCollectionItem & {
