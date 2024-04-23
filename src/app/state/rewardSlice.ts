7ff423066e228d813db56621f8529c5eb8b64fc9
Fred Liebenberg <fliebenberg@gmail.com>
2024-04-23 13:42:37 +0200

    removed resource_prefix from RewardConfig data

--- src/app/state/rewardSlice.ts
+++ src/app/state/rewardSlice.ts
@@ -21,7 +21,6 @@ export interface RewardState {
 }
 
 interface RewardConfig {
-  resourcePrefix: string;
   rewardComponent: string;
   rewardNFTAddress: string;
   rewardOrderAddress: string;
@@ -67,9 +66,6 @@ const initialState: RewardState = {
   },
   tokensList: [],
   config: {
-    resourcePrefix:
-      process.env.NEXT_PUBLIC_RESOURCE_PREFIX || "account_tdx_2_1",
-
     // this is the only address that will be specified in the .env file. All the other variables should be read from the component state variables
     rewardComponent:
       process.env.NEXT_PUBLIC_CLAIM_COMPONENT ||
