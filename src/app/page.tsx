9c29300eca887c7a7d7bafc7f36bb79d72d747e1
dcts <thomas@dcts.dev>
2024-05-05 00:23:08 +0200

    hero section draft done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-const containerWidthAndPadding = "w-[1200px] m-auto p-4 ";
+"use client";
+
+const containerWidthAndPadding = "w-[1200px] max-w-[100vw] m-auto p-4 ";
 
 export default function Landing() {
   return (
     <div className="bg-[#191B1D]">
+      <AnimatedLight />
       <HeaderSection />
       <Tokenomics />
       <EarnByTrading />
@@ -14,13 +17,15 @@ export default function Landing() {
 
 function HeaderSection() {
   return (
-    <div className={`bg-[url('/landing/background-nolights.png')]`}>
+    <div
+      className={`bg-[url('/landing/background-nolights.png')] bg-contain bg-no-repeat`}
+    >
       <div className={`${containerWidthAndPadding} h-[1100px]`}>
         {/* Hero Section */}
         <div className="flex mt-28">
           <div className="flex flex-col items-start justify-center max-w-[60%]">
             <h1 className="!m-0">Decentralized Order Book Exchange on Radix</h1>
-            <button>TRADE NOW</button>
+            <DexterButton title="TRADE NOW" />
           </div>
           <img
             src="/landing/dexter-mascotte.png"
@@ -53,5 +58,28 @@ function StakeToEarnDextr() {
 function EarnByContributing() {
   return (
     <div className={`${containerWidthAndPadding} `}>EarnByContributing</div>
+  );
+}
+
+interface DexterButtonProps {
+  title: string;
+}
+
+function DexterButton({ title }: DexterButtonProps) {
+  return (
+    <button
+      className={`max-w-[220px] min-h-[44px] uppercase w-full px-4 my-6 mt-8 rounded bg-dexter-green-OG text-black opacity-100`}
+      onClick={() => alert("Not implemented yet!")}
+    >
+      <span className="font-bold text-sm tracking-[.1px] ">{title}</span>
+    </button>
+  );
+}
+
+function AnimatedLight() {
+  return (
+    <div className="light-container">
+      <div className="animated-light"></div>
+    </div>
   );
 }
