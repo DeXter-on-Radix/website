0c3618c33b4e2518c264b06a2949ddf19c983cea
dcts <thomas@dcts.dev>
2024-05-07 18:02:49 +0200

    finish lights and structures positioning both mobile and desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+enum Device {
+  MOBILE = "MOBILE",
+  DESKTOP = "DESKTOP",
+}
+
 const containerWidthAndPadding = "w-[1200px] max-w-[100vw] m-auto p-8 ";
 
 export default function Landing() {
   return (
     <div className="bg-[#191B1D]">
-      <AnimatedLight />
-      <HeaderSection />
+      <HeroSection />
       <Tokenomics />
       <EarnByTrading />
       <StakeToEarnDextr />
@@ -15,59 +19,89 @@ export default function Landing() {
   );
 }
 
-function HeaderSection() {
+function HeroSection() {
   return (
-    <div
-    // className={`bg-[url('/landing/background-nolights.png')] bg-contain bg-no-repeat`}
-    >
-      <div
-        className={`${containerWidthAndPadding} min-h-[830px] h-[calc(100vh-74px)]`}
-      >
-        {/* Hero Section */}
+    <div>
+      <div className={`${containerWidthAndPadding} h-[calc(100vh-74px)]`}>
+        {/* Header Section */}
         <div className="flex justify-center mt-28 relative">
-          <BackgroundLightsAndStructures />
-          <div className="flex flex-col items-start justify-center min-[830px]:max-w-[60%] max-[830px]:items-center max-[830px]:text-center">
+          <div
+            className={
+              `flex flex-col items-start justify-center ` +
+              `min-[820px]:max-w-[60%] max-[820px]:items-center max-[820px]:text-center z-50 `
+            }
+          >
             <img
               src="/dexter-logo-and-lettering.svg"
               alt="dexter logo and lettering"
               className="pb-4 min-[420px]:hidden"
             />
-            <h1 className="!m-0 z-30 max-[830px]:max-w-[600px]">
+            <h1 className={`!m-0 z-100 max-[820px]:max-w-[600px]`}>
               Decentralized Order Book Exchange on Radix
             </h1>
-            <DexterButton title="TRADE NOW" targetUrl="/trade" />
+            <div className="relative">
+              <BackgroundLights type={Device.MOBILE} />
+              <DexterButton title="TRADE NOW" targetUrl="/trade" />
+            </div>
           </div>
-          <img
-            src="/landing/dexter-mascotte.png"
-            alt="Dexter Mascotte"
-            className="w-[300px] z-30 max-[830px]:hidden"
-          />
+          <div className="relative">
+            <BackgroundLights type={Device.DESKTOP} />
+            <img
+              src="/landing/dexter-mascotte.png"
+              alt="Dexter Mascotte"
+              className={`w-[300px] z-[100] max-[820px]:hidden relative `}
+            />
+          </div>
         </div>
         {/* Icons */}
       </div>
     </div>
   );
 }
-function BackgroundLightsAndStructures() {
-  return (
-    <>
-      <img
-        src="/landing/blue-light.png"
-        alt="blue light"
-        className="absolute top-[-218px] right-[-411px] z-20"
-      />
-      <img
-        src="/landing/green-light.png"
-        alt="green light"
-        className="absolute top-[-485px] right-[-112px] z-20"
-      />
-      <img
-        src="/landing/background-structures.png"
-        alt="background structures"
-        className="absolute w-[700px] opacity-10 z-10 top-[-213px] right-[-112px]"
-      />
-    </>
-  );
+
+function BackgroundLights({ type }: { type: Device }) {
+  if (type === Device.DESKTOP) {
+    return (
+      <>
+        <img
+          src="/landing/blue-light.png"
+          alt="blue light"
+          className="absolute opacity-60 z-10 scale-[4] top-[200px] right-[-239px] "
+        />
+        <img
+          src="/landing/green-light.png"
+          alt="green light"
+          className="absolute z-10 scale-[4] top-[-147px] right-[122px] "
+        />
+        <img
+          src="/landing/background-structures.png"
+          alt="background structures"
+          className="absolute opacity-10 z-20 scale-[2.5] top-[0px]"
+        />
+      </>
+    );
+  }
+  if (type === Device.MOBILE) {
+    return (
+      <div className="min-[820px]:hidden">
+        <img
+          src="/landing/blue-light.png"
+          alt="blue light"
+          className="absolute opacity-40 scale-[3] top-[-300%] left-[-50%] z-[-30] "
+        />
+        <img
+          src="/landing/green-light.png"
+          alt="green light"
+          className="absolute opacity-100 scale-[3] top-[-200%] right-[-80%] z-[-30] "
+        />
+        <img
+          src="/landing/background-structures.png"
+          alt="background structures"
+          className="absolute opacity-5 scale-[3] top-[-200%] right-[-50%] z-[-20] "
+        />
+      </div>
+    );
+  }
 }
 function Tokenomics() {
   return (
@@ -99,20 +133,12 @@ interface DexterButtonProps {
 
 function DexterButton({ title, targetUrl }: DexterButtonProps) {
   return (
-    <a href={targetUrl} className="z-50 w-full max-w-[220px]">
+    <a href={targetUrl} className="z-100 min-w-[220px] max-w-[220px]">
       <button
-        className={`max-w-[220px] min-h-[44px] uppercase w-full px-4 my-6 mt-8 rounded bg-dexter-green-OG text-black opacity-100 cursor-pointer`}
+        className={`min-h-[44px] w-[220px] uppercase px-4 my-6 mt-8 rounded bg-dexter-green-OG text-black opacity-100 cursor-pointer`}
       >
         <span className="font-bold text-sm tracking-[.1px] ">{title}</span>
       </button>
     </a>
-  );
-}
-
-function AnimatedLight() {
-  return (
-    <div className="light-container">
-      <div className="animated-light"></div>
-    </div>
   );
 }
