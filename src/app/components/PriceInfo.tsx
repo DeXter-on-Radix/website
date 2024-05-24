3e0d5cb781e3ce6d7c6a1cc53d1e13499e490f22
dcts <thomas@dcts.dev>
2024-05-24 02:08:39 +0200

    PriceInfo refactor + responsiveness

--- src/app/components/PriceInfo.tsx
+++ src/app/components/PriceInfo.tsx
@@ -17,69 +17,61 @@ export function PriceInfo() {
   const basePair = pairSelector.token2.symbol;
 
   return (
-    <div className="flex justify-between py-2 h-full max-w-[500px] min-[1026px]:m-auto">
-      <div className="flex flex-col items-start justify-center pl-5 lg:pl-8">
-        <span className="text-sm font-bold text-secondary-content uppercase">
-          {t("price")}
-        </span>
-        <span className={"text-sm font-bold text-primary-content"}>
-          {lastPrice}
-          <span className="text-sm font-bold text-secondary-content">
-            {" "}
-            {basePair}
-          </span>
-        </span>
-      </div>
-      <div className="flex flex-col items-start justify-center">
-        <span className="text-xs font-bold text-secondary-content uppercase pt-1">
-          {t("24h_change")}
-        </span>
-        <span
-          className={
-            isNegativeOrZero
-              ? "text-sm font-bold text-dexter-red"
-              : "text-sm font-bold text-dexter-green"
-          }
-        >
-          {isNegativeOrZero ? change : "+" + change}
-          <span className="text-sm font-bold text-secondary-content"> %</span>
-        </span>
-      </div>
-      <div className="flex flex-col items-start justify-center">
-        <span className="text-xs font-bold text-secondary-content uppercase pt-1">
-          {t("24h_volume")}
-        </span>
-        <span className="text-sm font-bold text-primary-content">
-          {volume}
-          <span className="text-sm font-bold text-secondary-content">
-            {" "}
-            {basePair}
-          </span>
-        </span>
-      </div>
-      <div className="flex flex-col items-start justify-center">
-        <span className="text-xs font-bold text-secondary-content uppercase pt-1">
-          {t("24h_high")}
-        </span>
-        <span className="text-sm font-bold text-primary-content">
-          {high}
-          <span className="text-sm font-bold text-secondary-content">
-            {" "}
-            {basePair}
-          </span>
-        </span>
-      </div>
-      <div className="flex flex-col items-start justify-center pr-5 lg:pr-8">
-        <span className="text-xs font-bold text-secondary-content uppercase pt-1">
-          {t("24h_low")}
-        </span>
-        <span className="text-sm font-bold text-primary-content">
-          {low}
-          <span className="text-sm font-bold text-secondary-content">
-            {" "}
-            {basePair}
-          </span>
-        </span>
+    <div className="flex flex-wrap justify-between items-center py-2 px-5 h-full max-w-[470px] min-[1026px]:m-auto max-[500px]:py-4 max-[500px]:justify-start">
+      <PriceInfoItem
+        title={t("price")}
+        valueStr={lastPrice}
+        label={basePair}
+        color={Color.NEUTRAL}
+      />
+      <PriceInfoItem
+        title={t("24h_change")}
+        valueStr={isNegativeOrZero ? `${change} %` : `+${change} %`}
+        label={""}
+        color={isNegativeOrZero ? Color.RED : Color.GREEN}
+      />
+      <PriceInfoItem
+        title={t("24h_volume")}
+        valueStr={volume}
+        label={basePair}
+        color={Color.NEUTRAL}
+      />
+      <PriceInfoItem
+        title={t("24h_high")}
+        valueStr={high}
+        label={basePair}
+        color={Color.NEUTRAL}
+      />
+      <PriceInfoItem
+        title={t("24h_low")}
+        valueStr={low}
+        label={basePair}
+        color={Color.NEUTRAL}
+      />
+    </div>
+  );
+}
+
+enum Color {
+  NEUTRAL = "text-primary-content",
+  RED = "text-dexter-red",
+  GREEN = "text-dexter-green",
+}
+
+interface PriceInfoItemProps {
+  title: string;
+  valueStr: string;
+  label: string;
+  color: Color;
+}
+
+function PriceInfoItem({ title, valueStr, label, color }: PriceInfoItemProps) {
+  return (
+    <div className="flex flex-col items-start justify-start max-[500px]:pr-5">
+      <div className="text-sm text-secondary-content pt-1">{title}</div>
+      <div className="flex">
+        <p className={`text-sm font-bold ${color} mr-1`}>{valueStr}</p>
+        <p className="text-sm text-secondary-content"> {label}</p>
       </div>
     </div>
   );
