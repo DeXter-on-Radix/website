4d14bac41890d6a239c06deaaafce23786c7d25f
dcts <thomas@dcts.dev>
2024-05-31 03:17:11 +0200

    adapt orderbook tests to reflect 13 rows instead of 11

--- src/app/state/orderBookSlice.test.ts
+++ src/app/state/orderBookSlice.test.ts
@@ -1,6 +1,6 @@
 import { OrderbookLine } from "alphadex-sdk-js";
 import {
-  // OrderBookRowProps,
+  OrderBookRowProps,
   toOrderBookRowProps,
   MAX_ROWS,
 } from "./orderBookSlice";
@@ -9,6 +9,7 @@ function mockOrderBookLine(price: number): OrderbookLine {
 }
 
 const MOCK_SELLS = [
+  mockOrderBookLine(26),
   mockOrderBookLine(25),
   mockOrderBookLine(24),
   mockOrderBookLine(23),
@@ -23,21 +24,21 @@ const MOCK_SELLS = [
   mockOrderBookLine(14),
 ];
 
-// const MOCK_BUYS = [
-//   mockOrderBookLine(13),
-//   mockOrderBookLine(12),
-//   mockOrderBookLine(11),
-//   mockOrderBookLine(10),
-//   mockOrderBookLine(9),
-//   mockOrderBookLine(8),
-//   mockOrderBookLine(7),
-//   mockOrderBookLine(6),
-//   mockOrderBookLine(5),
-//   mockOrderBookLine(4),
-//   mockOrderBookLine(3),
-//   mockOrderBookLine(2),
-//   mockOrderBookLine(1),
-// ];
+const MOCK_BUYS = [
+  mockOrderBookLine(13),
+  mockOrderBookLine(12),
+  mockOrderBookLine(11),
+  mockOrderBookLine(10),
+  mockOrderBookLine(9),
+  mockOrderBookLine(8),
+  mockOrderBookLine(7),
+  mockOrderBookLine(6),
+  mockOrderBookLine(5),
+  mockOrderBookLine(4),
+  mockOrderBookLine(3),
+  mockOrderBookLine(2),
+  mockOrderBookLine(1),
+];
 
 describe("toOrderBookRowProps", () => {
   it(`returns correct amount of rows if the input is empty`, () => {
@@ -52,89 +53,95 @@ describe("toOrderBookRowProps", () => {
     expect(toOrderBookRowProps(input, "sell", 0).length).toBe(MAX_ROWS);
   });
 
-  it(`"returns correct amount of rows if the input is larger"`, () => {
+  it(`returns correct amount of rows if the input is larger`, () => {
     expect(toOrderBookRowProps(MOCK_SELLS, "sell", 0).length).toBe(MAX_ROWS);
   });
 
-  // it("drops the correct farther away rows for sells", () => {
-  //   const expectedSellPrices = [24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14];
+  it("drops the correct farther away rows for sells", () => {
+    const expectedSellPrices = [
+      26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14,
+    ];
 
-  //   const sellRowProps = toOrderBookRowProps(MOCK_SELLS, "sell", 0);
+    const sellRowProps = toOrderBookRowProps(MOCK_SELLS, "sell", 0);
 
-  //   sellRowProps.forEach((row: OrderBookRowProps, index: number) => {
-  //     expect(row.price).toBe(expectedSellPrices[index]);
-  //   });
-  // });
+    sellRowProps.forEach((row: OrderBookRowProps, index: number) => {
+      expect(row.price).toBe(expectedSellPrices[index]);
+    });
+  });
 
-  // it("drops the correct farther away rows for buys", () => {
-  //   const expectedBuyPrices = [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3];
+  it("drops the correct farther away rows for buys", () => {
+    const expectedBuyPrices = [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
-  //   const buyRowProps = toOrderBookRowProps(MOCK_BUYS, "buy", 0);
+    const buyRowProps = toOrderBookRowProps(MOCK_BUYS, "buy", 0);
 
-  //   buyRowProps.forEach((row: OrderBookRowProps, index: number) => {
-  //     expect(row.price).toBe(expectedBuyPrices[index]);
-  //   });
-  // });
+    buyRowProps.forEach((row: OrderBookRowProps, index: number) => {
+      expect(row.price).toBe(expectedBuyPrices[index]);
+    });
+  });
 
-  // it("calculates correct totals", () => {
-  //   const expectedSellTotals = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-  //   const expectedBuyTotals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  it("calculates correct totals", () => {
+    const expectedSellTotals = [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+    const expectedBuyTotals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
-  //   const sellRowProps = toOrderBookRowProps(MOCK_SELLS, "sell", 0);
-  //   const buyRowProps = toOrderBookRowProps(MOCK_BUYS, "buy", 0);
+    const sellRowProps = toOrderBookRowProps(MOCK_SELLS, "sell", 0);
+    const buyRowProps = toOrderBookRowProps(MOCK_BUYS, "buy", 0);
 
-  //   sellRowProps.forEach((row: OrderBookRowProps, index: number) => {
-  //     expect(row.total).toBe(expectedSellTotals[index]);
-  //   });
+    sellRowProps.forEach((row: OrderBookRowProps, index: number) => {
+      expect(row.total).toBe(expectedSellTotals[index]);
+    });
 
-  //   buyRowProps.forEach((row: OrderBookRowProps, index: number) => {
-  //     expect(row.total).toBe(expectedBuyTotals[index]);
-  //   });
-  // });
+    buyRowProps.forEach((row: OrderBookRowProps, index: number) => {
+      expect(row.total).toBe(expectedBuyTotals[index]);
+    });
+  });
 
-  // it("groups the sell rows correctly", () => {
-  //   const expected = [
-  //     { price: undefined, total: undefined },
-  //     { price: undefined, total: undefined },
-  //     { price: undefined, total: undefined },
-  //     { price: undefined, total: undefined },
-  //     { price: undefined, total: undefined },
-  //     { price: undefined, total: undefined },
-  //     { price: undefined, total: undefined },
-  //     { price: 24, total: 12 },
-  //     { price: 21, total: 9 },
-  //     { price: 18, total: 6 },
-  //     { price: 15, total: 3 },
-  //   ];
+  it("groups the sell rows correctly", () => {
+    const expected = [
+      { price: undefined, total: undefined },
+      { price: undefined, total: undefined },
+      { price: undefined, total: undefined },
+      { price: undefined, total: undefined },
+      { price: undefined, total: undefined },
+      { price: undefined, total: undefined },
+      { price: undefined, total: undefined },
+      { price: undefined, total: undefined },
+      { price: 27, total: 13 },
+      { price: 24, total: 12 },
+      { price: 21, total: 9 },
+      { price: 18, total: 6 },
+      { price: 15, total: 3 },
+    ];
 
-  //   const sellRowProps = toOrderBookRowProps(MOCK_SELLS, "sell", 3);
+    const sellRowProps = toOrderBookRowProps(MOCK_SELLS, "sell", 3);
 
-  //   sellRowProps.forEach((row: OrderBookRowProps, index: number) => {
-  //     expect(row.price).toBe(expected[index].price);
-  //     expect(row.total).toBe(expected[index].total);
-  //   });
-  // });
+    sellRowProps.forEach((row: OrderBookRowProps, index: number) => {
+      expect(row.price).toBe(expected[index].price);
+      expect(row.total).toBe(expected[index].total);
+    });
+  });
 
-  // it("groups the buy rows correctly", () => {
-  //   const expected = [
-  //     { price: 12, total: 3 },
-  //     { price: 9, total: 6 },
-  //     { price: 6, total: 9 },
-  //     { price: 3, total: 12 },
-  //     { price: undefined, total: undefined },
-  //     { price: undefined, total: undefined },
-  //     { price: undefined, total: undefined },
-  //     { price: undefined, total: undefined },
-  //     { price: undefined, total: undefined },
-  //     { price: undefined, total: undefined },
-  //     { price: undefined, total: undefined },
-  //   ];
+  it("groups the buy rows correctly", () => {
+    const expected = [
+      { price: 12, total: 3 },
+      { price: 9, total: 6 },
+      { price: 6, total: 9 },
+      { price: 3, total: 12 },
+      { price: undefined, total: undefined },
+      { price: undefined, total: undefined },
+      { price: undefined, total: undefined },
+      { price: undefined, total: undefined },
+      { price: undefined, total: undefined },
+      { price: undefined, total: undefined },
+      { price: undefined, total: undefined },
+      { price: undefined, total: undefined },
+      { price: undefined, total: undefined },
+    ];
 
-  //   const buyRowProps = toOrderBookRowProps(MOCK_BUYS, "buy", 3);
+    const buyRowProps = toOrderBookRowProps(MOCK_BUYS, "buy", 3);
 
-  //   buyRowProps.forEach((row: OrderBookRowProps, index: number) => {
-  //     expect(row.price).toBe(expected[index].price);
-  //     expect(row.total).toBe(expected[index].total);
-  //   });
-  // });
+    buyRowProps.forEach((row: OrderBookRowProps, index: number) => {
+      expect(row.price).toBe(expected[index].price);
+      expect(row.total).toBe(expected[index].total);
+    });
+  });
 });
