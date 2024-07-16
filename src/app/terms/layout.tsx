f5e7a23a9cbd00faceeffbee59cfdd0ffbcb625d
saidam90 <saida.murtazaliyeva@yahoo.com>
2024-07-16 21:25:59 +0400

    Fix max width to a medium screen size

--- src/app/terms/layout.tsx
+++ src/app/terms/layout.tsx
@@ -2,7 +2,7 @@ export default function MdxLayout({ children }: { children: React.ReactNode }) {
   // Create any shared layout or styles here
   return (
     <div className="px-16">
-      <div className="max-w-screen-lg mx-auto py-28">
+      <div className="max-w-screen-md mx-auto py-28">
         <h1
           className="!m-0 !mb-8 text-5xl text-md bg-gradient-to-r
           from-dexter-gradient-blue to-dexter-gradient-green to-50% bg-clip-text
