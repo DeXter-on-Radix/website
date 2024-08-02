ecb6d190c87412209ebf733f11b19639e6a197be
dcts <thomas@dcts.dev>
2024-08-02 23:04:04 +0200

    fix linter errors

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, useTransition } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
