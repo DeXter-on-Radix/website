0e8b487877b1c5abf218cddbee8510213d87b805
saidam90 <saida.murtazaliyeva@yahoo.com>
2024-08-10 19:10:31 +0400

    Add tooltip to confirm the copy action

--- src/app/components/CopyIcon.tsx
+++ src/app/components/CopyIcon.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
+import { useAppDispatch } from "../hooks";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { MdContentCopy } from "react-icons/md";
 import { setCopied } from "../state/priceChartSlice";
-import { DexterToast } from "components/DexterToaster";
-import { useAppDispatch } from "../hooks";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/dist/svg-arrow.css";
 
 interface CopyIconProps {
   targetUrl: string;
@@ -10,24 +13,37 @@ interface CopyIconProps {
 
 export function CopyIcon({ targetUrl }: CopyIconProps) {
   const dispatch = useAppDispatch();
+  const [visible, setVisible] = useState(false);
 
   const handleCopy = () => {
     dispatch(setCopied(true));
-    DexterToast.success("Copied!");
+    setVisible(true);
 
     setTimeout(() => {
+      setVisible(false);
       dispatch(setCopied(false));
-    }, 2000);
+    }, 700);
   };
 
   return (
     <>
-      <CopyToClipboard text={targetUrl} onCopy={handleCopy}>
-        <MdContentCopy
-          className="cursor-pointer text-base"
-          title="Copy to clipboard"
-        />
-      </CopyToClipboard>
+      <Tippy
+        content={<span>Copied!</span>}
+        visible={visible}
+        onClickOutside={() => setVisible(false)}
+        arrow={true}
+        theme="custom"
+      >
+        <div>
+          <CopyToClipboard text={targetUrl} onCopy={handleCopy}>
+            <MdContentCopy
+              className="cursor-pointer text-base"
+              title="Copy to clipboard"
+              onClick={() => setVisible(true)}
+            />
+          </CopyToClipboard>
+        </div>
+      </Tippy>
     </>
   );
 }
