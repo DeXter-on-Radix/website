34a17352fc562758b920726dbc543b95b07d3d87
Pratham-Gaikwad <prathamgaikwad2011@gmail.com>
2024-08-12 16:17:18 +0530

    Fixed Mobile NavBar text and Translation .

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -332,6 +332,7 @@ function NavbarItemMobile({
   setMenuOpen,
 }: NavbarItemMobileProps) {
   const active = target === usePathname();
+  const translate = useTranslations();
   return (
     <Link
       className={`my-2 hover:!no-underline`}
@@ -343,7 +344,7 @@ function NavbarItemMobile({
           active ? "text-[#cafc40]" : "text-white"
         }`}
       >
-        {title}
+        {translate(title)}
       </p>
     </Link>
   );
