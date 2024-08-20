bdbc2d5b5d55315c8f97b782b2040415f885730f
Thomas Starzynski <dcts@google.com>
2024-08-20 21:30:32 +0200

    fix linter error

--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
 const Roadmap = () => {
   const router = useRouter();
