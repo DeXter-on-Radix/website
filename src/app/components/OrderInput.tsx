04dcee33c9323a1675c927fa820674ad04dd61cc
Thomas <thomas@dcts.dev>
2024-09-21 00:42:16 +0200

    fix linter errors

--- src/app/components/OrderInput.tsx
+++ src/app/components/OrderInput.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, ChangeEvent } from "react";
+import { useEffect, useState, useRef, ChangeEvent, useCallback } from "react";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css";
@@ -472,45 +472,52 @@ function UserInputContainer() {
   const isBuyOrder = side === "BUY";
   const isSellOrder = side === "SELL";
 
+  const sliderCallback = useCallback(
+    (newPercentage: number) => {
+      const isXRDToken = isBuyOrder
+        ? token2.symbol === "XRD"
+        : token1.symbol === "XRD";
+      let balance = isBuyOrder ? balanceToken2 : balanceToken1;
+
+      if (newPercentage === 100 && isXRDToken) {
+        balance = Math.max(balance - XRD_FEE_ALLOWANCE, 0);
+      }
+
+      const amount = Calculator.divide(
+        Calculator.multiply(balance, newPercentage),
+        100
+      );
+
+      const specifiedToken = isBuyOrder
+        ? SpecifiedToken.TOKEN_2
+        : SpecifiedToken.TOKEN_1;
+
+      dispatch(
+        orderInputSlice.actions.setTokenAmount({
+          amount,
+          bestBuy,
+          bestSell,
+          balanceToken1,
+          balanceToken2,
+          specifiedToken,
+        })
+      );
+    },
+    [
+      isBuyOrder,
+      token1.symbol,
+      token2.symbol,
+      balanceToken1,
+      balanceToken2,
+      bestBuy,
+      bestSell,
+      dispatch,
+    ]
+  );
+
   useEffect(() => {
-    if (isMarketOrder) {
-      sliderCallback(0, "MARKET");
-    } else if (isLimitOrder) {
-      sliderCallback(0, "LIMIT");
-    }
-  }, [isBuyOrder, isSellOrder, isMarketOrder, isLimitOrder]);
-
-  const sliderCallback = (newPercentage: number, type: string) => {
-    console.log(`Type is: ${type}`);
-    const isXRDToken = isBuyOrder
-      ? token2.symbol === "XRD"
-      : token1.symbol === "XRD";
-    let balance = isBuyOrder ? balanceToken2 : balanceToken1;
-
-    if (newPercentage === 100 && isXRDToken) {
-      balance = Math.max(balance - XRD_FEE_ALLOWANCE, 0);
-    }
-
-    const amount = Calculator.divide(
-      Calculator.multiply(balance, newPercentage),
-      100
-    );
-
-    const specifiedToken = isBuyOrder
-      ? SpecifiedToken.TOKEN_2
-      : SpecifiedToken.TOKEN_1;
-
-    dispatch(
-      orderInputSlice.actions.setTokenAmount({
-        amount,
-        bestBuy,
-        bestSell,
-        balanceToken1,
-        balanceToken2,
-        specifiedToken,
-      })
-    );
-  };
+    sliderCallback(0);
+  }, [isBuyOrder, isSellOrder, isMarketOrder, isLimitOrder, sliderCallback]);
 
   return (
     <div className="bg-base-100 px-5 pb-5 rounded-b">
@@ -523,7 +530,7 @@ function UserInputContainer() {
           <PercentageSlider
             initialPercentage={0}
             callbackOnPercentageUpdate={(newPercentage) =>
-              sliderCallback(newPercentage, type)
+              sliderCallback(newPercentage)
             }
             isLimitOrder={isLimitOrder}
             isBuyOrder={isBuyOrder}
@@ -544,7 +551,7 @@ function UserInputContainer() {
           <PercentageSlider
             initialPercentage={0}
             callbackOnPercentageUpdate={(newPercentage) =>
-              sliderCallback(newPercentage, type)
+              sliderCallback(newPercentage)
             }
             isLimitOrder={isLimitOrder}
             isBuyOrder={isBuyOrder}
